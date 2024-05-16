b04911b6f0e66d89d49bec0dba62290da317cff5
Bogdan Maria <maria_costinel@yahoo.com>
2024-05-16 13:32:54 +0000

    Add partial and static folder

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
 
 # Create your tests here.
+class TestHomeView(TestCase):
+
+    def test_home_page_loads(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
