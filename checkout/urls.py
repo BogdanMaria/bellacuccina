bb634f0207e5be7d105833ff0002851b935deb85
Bogdan Maria <maria_costinel@yahoo.com>
2024-05-30 10:07:41 +0000

    Add checkout form data caching

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from .webhooks import webhook
 
 from . import views
 
@@ -8,7 +9,8 @@ app_name = 'checkout'
 
 urlpatterns = [
     path('checkout/', views.store_checkout, name='store_checkout'),
-    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success')
-
+    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
 
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -5,11 +5,30 @@ from django.http import JsonResponse
 from django.conf import settings
 import math
 import stripe
+import json
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
 from shopping_cart.context import cart_content
+
+
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metdata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request,
+                        'Sorry we cant process your payment at the moment\
+                        plaease try later!')
+        return HttpResponse(status=400)
+
 
 def store_checkout(request):
 
