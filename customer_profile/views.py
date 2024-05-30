962b7056296294630a1fc7752714b8d665806752
Bogdan Maria <maria_costinel@yahoo.com>
2024-05-30 11:14:31 +0000

    Add basic file view url and template for customer profile

--- customer_profile/views.py
+++ customer_profile/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import (render, redirect,
+                              reverse, HttpResponse, get_object_or_404)
+from django.contrib import messages
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse
+from django.conf import settings
 
-# Create your views here.
+
+def profile(request):
+    """
+    view handling user profile
+    """
+    print('view called')
+    template = 'customer_profile/profile.html'
+    context = {}
+    return render(request, template, context)
