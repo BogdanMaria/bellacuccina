c0f91c020e3e5daf4c9a59b7542f744d8cf428d4
Bogdan Maria <maria_costinel@yahoo.com>
2024-05-30 13:20:47 +0000

    Fix all products template

--- customer_profile/views.py
+++ customer_profile/views.py
@@ -5,12 +5,22 @@ from django.views.decorators.http import require_POST
 from django.http import JsonResponse
 from django.conf import settings
 
+from customer_profile.models import CustomerProfile
+from .forms import CustomerProfileForm
+from checkout.models import Order
+
 
 def profile(request):
     """
     view handling user profile
     """
-    print('view called')
+    customer_profile = get_object_or_404(CustomerProfile, user=request.user)
+    form = CustomerProfileForm(instance=customer_profile)
+    orders = customer_profile.orders.all()
     template = 'customer_profile/profile.html'
-    context = {}
+    context = {
+        'form': form,
+        'orders': orders,
+        'customer_profile': customer_profile,
+    }
     return render(request, template, context)
