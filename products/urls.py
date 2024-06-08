ab572c616724e64a97a79ffa96bb2c35621705f3
Bogdan Maria <maria_costinel@yahoo.com>
2024-06-08 10:27:26 +0000

    Add edit product functionality for admin user

--- products/urls.py
+++ products/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('products/', views.store_products, name='store_products'),
     path('products/<int:product_id>/', views.product_detail, name='product_detail'),
     path('add_product/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 
 ]
