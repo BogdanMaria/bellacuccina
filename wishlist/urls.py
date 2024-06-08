fc2fc578c51549e6b3890fbe21d7691a51c0079a
Bogdan Maria <maria_costinel@yahoo.com>
2024-06-08 09:15:50 +0000

    Added add top shopping cart functionality from wishlist page and added skeletom for basic add to cart custom modal

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -7,7 +7,7 @@ from . import views
 app_name = 'wishlist'
 
 urlpatterns = [
-    path('', views.wishlist, name='wishlist '),
+    path('', views.wishlist, name='wishlist'),
     path('add_to_wishlist/', views.add_to_wishlist, name='add_to_wishlist'),
     path('remove_from_wishlist/', views.remove_from_wishlist, name='remove_from_wishlist'),
 ]
