08960f51c1885763af9f0664bedc42d84270b67d
Bogdan Maria <maria_costinel@yahoo.com>
2024-06-13 15:15:41 +0000

    Add functionality for user to check order history from his profile page

--- customer_profile/urls.py
+++ customer_profile/urls.py
@@ -7,4 +7,5 @@ app_name = 'customer_profile'
 
 urlpatterns = [
     path('', views.profile, name='profile'),
+    path('order_history/<order_number>/', views.order_history, name='order_history'),
 ]
