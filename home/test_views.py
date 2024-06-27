e922c37a0c3787365c62657cccccb301e260c741
Bogdan Maria <maria_costinel@yahoo.com>
2024-06-27 11:59:47 +0000

    Add automated tests(Unittest) for home app

---
+++ home/test_views.py
@@ -0,0 +1,18 @@
+"""
+home views test module
+"""
+
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
+from customer_profile.models import User
+
+
+class TestHomeView(TestCase):
+
+    def test_home_page_loads(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_load_right_template(self):
+        response = self.client.get('')
+        self.assertTemplateUsed(response, 'home/index.html')
