884703fe5a6a9a260e0df5a307a03531b19be1ce
Bogdan Maria <maria_costinel@yahoo.com>
2024-06-27 16:40:49 +0000

    Add automated test for product views

--- wishlist/test_views.py
+++ wishlist/test_views.py
@@ -81,7 +81,7 @@ class TestAddingToWishlist(TestCase):
         self.user.save()
 
         self.my_category = Category.objects.create(
-            name='Savage',
+            name='Pasta',
             notes='test notes',
             slug='testslug',
             friendly_name='Pasta'
@@ -99,16 +99,64 @@ class TestAddingToWishlist(TestCase):
         )
         self.product.save()
 
-    def test_removing_from_wishlist(self):
-        self.client.login(username='MyTestUser', password='mypass79')
-        if self.client._login:
-            print("User is logged in")
-        else:
-            print("User is not logged in")
+    def test_add_to_wishlist(self):
+        self.client = Client()
+        self.client.force_login(self.user)
+        self.redirect_url = reverse(('products:store_products'))
+        response = self.client.post('/wishlist/add_to_wishlist/', data={
+            'product-id': self.product.id,
+            'my_redirect_url': self.redirect_url
 
-        print(f"Product ID: {self.product.id}")
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Wishlist.objects.count(), 1)
+        self.assertRedirects(response, reverse('products:store_products'))
+
+
+
+class TestRemovingFromWishlist(TestCase):
+
+    @classmethod
+    def setUp(self):
+        """
+        creating and saving a new test user
+        """
+
+        self.user = User.objects.create(
+            username='MyTestUser',
+            password='mypass79',
+            email='test@user.com',
+            id='1',
+        )
+        self.user.save()
+        self.user.set_password('mypass799')
+        self.user.save()
+
+        self.my_category = Category.objects.create(
+            name='Pasta',
+            notes='test notes',
+            slug='testslug',
+            friendly_name='Pasta'
+        )
+        self.my_category.save()
+
+        self.product = Product.objects.create(
+            category=self.my_category,
+            item_no='A221',
+            name='Pasta',
+            description='Test pasta description',
+            price=5.00,
+            weight=500,
+            id=23,
+        )
+        self.product.save()
+
+    def test_removing_from_wishlist(self):
+        self.client = Client()
+        self.client.force_login(self.user)
         wishlist_item = Wishlist.objects.create(user=self.user, product_id=23)
         wishlist_item_id = wishlist_item.id
         response = self.client.post('/wishlist/remove_from_wishlist/', data={'item-id': wishlist_item_id})
         print(f"Wishlist count: {Wishlist.objects.count()}")
         self.assertEqual(response.status_code, 302)
+        self.assertEqual(Wishlist.objects.count(), 0)
