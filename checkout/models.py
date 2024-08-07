1b3bad2e2c951ad5ea63960eda7770774c8225a0
Bogdan Maria <maria_costinel@yahoo.com>
2024-08-07 13:39:11 +0000

    Change the order number field length

--- checkout/models.py
+++ checkout/models.py
@@ -9,7 +9,7 @@ from customer_profile.models import CustomerProfile
 
 
 class Order(models.Model):
-    order_number = models.CharField(max_length=32, null=False, editable=False)
+    order_number = models.CharField(max_length=34, null=False, editable=False)
     user_profile = models.ForeignKey(CustomerProfile,
                                      on_delete=models.SET_NULL, null=True,
                                      blank=True, related_name='orders')
