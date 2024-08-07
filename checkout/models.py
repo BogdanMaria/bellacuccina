8075f15feec8fa900c668d3e45223bd5c7679d73
Bogdan Maria <maria_costinel@yahoo.com>
2024-08-07 16:56:29 +0000

    modify the checkout model

--- checkout/models.py
+++ checkout/models.py
@@ -57,7 +57,6 @@ class Order(models.Model):
     def __str__(self):
         return self.order_number
 
-
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, 
                               on_delete=models.CASCADE, related_name='lineitems')
